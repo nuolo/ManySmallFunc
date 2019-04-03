f71ee2c348cbf60a2614a6fb57f36b45dc6bda7a
nuolo <1757688586@qq.com>
2019-04-03 17:54:37 +0800

    23
    23

---
+++ Assets/Test 23刷新Layout/GridLayoutTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridLayoutTest : MonoBehaviour
+{
+  
+    void Start()
+    {
+
+    }
+    [ContextMenu("Chang")]
+    void ChangLayout()
+    {
+        GetComponent<GridLayoutGroup>().constraintCount = 3;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(this.GetComponent<RectTransform>());
+    }
+}
